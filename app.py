fdf15baa3aa488127425df054018c873eab95971
AKASH GUPTA <113404032+Akash140605@users.noreply.github.com>
2026-01-21 22:40:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ app = Flask(__name__)
 # CORS Configuration
 CORS(app, resources={
     r"/api/*": {
-        "origins": ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+        "origins": ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://phasalaipr.onrender.com"],
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type"]
     }
@@ -358,3 +358,4 @@ if __name__ == "__main__":
     # Production (uncomment and use gunicorn instead)
     # from waitress import serve
     # serve(app, host='0.0.0.0', port=8080)
+
